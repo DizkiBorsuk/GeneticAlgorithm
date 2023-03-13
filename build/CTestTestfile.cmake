fd120815e2ddd58113be45e320e2ae03d82f24f1
Maciek Rostocki <01130216@pw.edu.pl>
2023-03-13 21:34:00 +0100

    change to cmake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Maciek/Desktop/dev_workspace/Projects/GeneticAlgorithm
+# Build directory: C:/Users/Maciek/Desktop/dev_workspace/Projects/GeneticAlgorithm/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- main.cpp
+++ main.cpp
@@ -46,7 +46,6 @@ int main()
 	std::cin >> desired_outcome;
 	std::cout << "\n"; 
 	
-	double rank = 0; 
 
 	while (true)
 	{ 
